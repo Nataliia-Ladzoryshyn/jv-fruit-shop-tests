8e6972d2e7f832f0932fbab717c0b2565cf0ff75
Nataliia Ladzoryshyn <122207731+Nataliia-Ladzoryshyn@users.noreply.github.com>
2025-12-15 20:05:37 +0200

    Revert "Hm jv fruit shop tests"

--- src/main/java/core/basesyntax/service/FileRead.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax.service;
-
-import java.util.List;
-
-public interface FileRead {
-    List<String> readFileContents(String filePath);
-}
--- src/main/java/core/basesyntax/service/FileWriter.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax.service;
-
-import java.util.List;
-
-public interface FileWriter {
-    void write(List<String> data, String filePath);
-}
